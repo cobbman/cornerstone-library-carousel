85333f06668f00990c14b4cadacde83b47332a0b
William <hello@bigwilliam.com>
2015-11-30 16:22:17 -0700

    remove excess js, owl.zip and gitmodules

--- assets/js/cornerstone-carousel.js
+++ assets/js/cornerstone-carousel.js
@@ -1,19 +1,3 @@
-// Vars
-// 1. unique ID
-// 2. max items
-// 3. duration
-
-// jQuery(document).ready(function($) {
-//   $('.cornerstone-carousel').carouFredSel({
-//     items      : { 
-//       visible: { min: 1, max: 4 } 
-//     },
-//     scroll : {
-//       items         : 1,
-//       easing        : 'easeInOutQuint',
-//       duration      : 700,
-//       pauseOnHover  : false
-//     },
-//     responsive : true
-//   });
-// });
+jQuery(document).ready(function($) {
+	
+});
