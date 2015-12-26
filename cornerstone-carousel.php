6757c4672a776212463a97f1ff11776902801518
William <hello@bigwilliam.com>
2015-12-25 20:22:30 -0700

    change main file with new PHP calls

--- cornerstone-carousel.php
+++ cornerstone-carousel.php
@@ -3,7 +3,7 @@
 Plugin Name: Cornerstone Library: Carousel
 Plugin URI:  http://bigwilliam.com/
 Description: Adds a horizontal scrolling carousel to the Cornerstone Page builder. Download plugin at <a href="http://cornerstonelibrary.com" target="_blank">Cornerstone Library</a>.
-Version:     1.2
+Version:     2.0
 Author:      William
 Author URI:  http://bigwilliam.com
 Author Email: william@d3fy.com
@@ -15,37 +15,28 @@ Text Domain: __x__
 // Prevent direct access
 if ( ! defined( 'WPINC' ) ) die;
 
+define( 'CSL_CAROUSEL_PATH', plugin_dir_path( __FILE__ ) );
+define( 'CSL_CAROUSEL_URL', plugin_dir_url( __FILE__ ) );
 
 
 /*
  * => ENQUEUE SCRIPTS
  * ---------------------------------------------------------------------------*/
-function cornerstone_carousel_scripts() {
+function csl_carousel_scripts() {
 
 	// OWL CAROUSEL
-	wp_enqueue_script( 'owl-carousel-js', plugins_url('/assets/js/owl-carousel/owl.carousel.min.js', __FILE__ ), array('jquery'), null, true );
-	wp_enqueue_style( 'owl-main-css', plugins_url('/assets/js/owl-carousel/owl.carousel.css', __FILE__ ), array(), '1.2' );
-	wp_enqueue_style( 'owl-theme-css', plugins_url('/assets/js/owl-carousel/owl.theme.css', __FILE__ ), array(), '1.2' );
-
-	// CUSTOM
-	// wp_enqueue_script( 'cornerstone-carousel-js', plugins_url('/assets/js/cornerstone-carousel.js', __FILE__ ), array('jquery'), null, true );
-	// wp_enqueue_style('cornerstone-carousel-css', plugins_url('/assets/css/cornerstone-carousel.css', __FILE__ ), array(), '1.0' );
+	wp_enqueue_script( 'owl-carousel-js', CSL_CAROUSEL_URL . '/assets/js/owl-carousel/owl.carousel.min.js', array('jquery'), null, true );
+	wp_enqueue_style( 'owl-main-css', CSL_CAROUSEL_URL . '/assets/js/owl-carousel/owl.carousel.css', array(), '1.2' );
+	wp_enqueue_style( 'owl-theme-css', CSL_CAROUSEL_URL . '/assets/js/owl-carousel/owl.theme.css', array(), '1.2' );
 }
-add_action( 'wp_enqueue_scripts', 'cornerstone_carousel_scripts', 100 );
+add_action( 'wp_enqueue_scripts', 'csl_carousel_scripts');
 
 
 /*
- * => SHORTCODES
+ * => ADD TO CORNERSTONE
  * ---------------------------------------------------------------------------*/
-
-require_once( 'shortcodes/cornerstone-carousel-shortcodes.php' );
-
-
-/*
- * => ADD ELEMENTS TO CORNERSTONE
- * ---------------------------------------------------------------------------*/
-function cornerstone_carousel_elements() {
+function csl_carousel_elements() {
 	require_once( 'elements/cornerstone-carousel-element.php' );
 	require_once( 'elements/cornerstone-carousel-element-item.php' );
 }
-add_action( 'cornerstone_load_elements', 'cornerstone_carousel_elements' );
+add_action( 'cornerstone_register_elements', 'csl_carousel_elements' );
