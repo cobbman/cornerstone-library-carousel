81faf13a9f0b4be9a63e4f7468f89f7db4702a40
William <hello@bigwilliam.com>
2016-01-01 16:36:57 -0700

    item controls

---
+++ includes/csl-carousel-item/controls.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Element Controls: CSL Carousel ITEM
+ */
+
+return array(
+
+	// Title
+
+	'title' => array(
+		'type' => 'title',
+		'context' => 'content',
+		'suggest' => __( 'Carousel Item', '__x__' ),
+	),
+
+	// Content
+
+	'content' => array(
+		'type' => 'editor',
+		'context' => 'content',
+		'suggest' => __( 'Add an image, shortcode, or text here! Button links also work!', '__x__' ),
+	)
+)
