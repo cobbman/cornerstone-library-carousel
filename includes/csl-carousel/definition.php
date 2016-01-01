863f65cfa0eca4057c40ce9e324792a953583dc9
William <hello@bigwilliam.com>
2016-01-01 15:48:18 -0700

    add new definitions

---
+++ includes/csl-carousel/definition.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Definition: Cornerstone Carousel
+ */
+
+class CSL_Carousel {
+
+	public function ui() {
+		return array(
+			'title' => __( 'Carousel', '__x__' )
+		);
+	}
+
+	public function flags() {
+		// dynamic_child allows child elements to render individually, but may cause
+		// styling or behavioral issues in the page builder depending on how your
+		// shortcodes work. If you have trouble with element presentation, try
+		// removing this flag.
+		return array(
+			'dynamic_child' => true
+		);
+	}
+	
+}
